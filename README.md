387b143939e76bdc51a9472429730d431d1b9efd
johngiorgi <johnmgiorgi@gmail.com>
2021-02-11 20:00:04 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# athology-ml
+# Athology-ML
+
+This repository contains our back-end, which exposes several REST API endpoints for computation and prediction on accelerometer data.
+
+## Installation
+
+This repository requires Python 3.8 or later. Installation is managed with [Poetry](https://python-poetry.org/).
+
+```bash
+# Install poetry for your system: https://python-poetry.org/docs/#installation
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python
+
+# Clone and move into the repo
+git clone https://github.com/dcsil/athology-ml
+cd athology_ml
+
+# Install the package with poetry
+poetry install
+```
+
+## Usage
+
+To start the web service, run
+
+```bash
+uvicorn athology_ml.main:app \
+    --host 0.0.0.0 \
+    --port 5000 \
+    --reload
+```
+
+For a health check on a running service, run
+
+```
+curl -X GET "http://localhost:5000/" 
+```
+
+For documentation, visit [`http://localhost:5000/docs`](http://localhost:5000/docs) in your browser.
---
+++ athology_ml/main.py
@@ -0,0 +1,48 @@
+from functools import wraps
+from http import HTTPStatus
+from datetime import datetime
+
+from fastapi import FastAPI, Request
+
+app = FastAPI(
+    title="Athology Backend and ML Web Services",
+    description="Exposes several REST API endpoints for computation and prediction on accelerometer data.",
+    version="0.1.0",
+)
+
+
+def construct_response(f):
+    """Construct a JSON response for an endpoint's results."""
+
+    @wraps(f)
+    def wrap(request: Request, *args, **kwargs):
+        results = f(request, *args, **kwargs)
+
+        # Construct response
+        response = {
+            "message": results["message"],
+            "method": request.method,
+            "status-code": results["status-code"],
+            "timestamp": datetime.now().isoformat(),
+            "url": request.url._url,
+        }
+
+        # Add data
+        if "data" in results:
+            response["data"] = results["data"]
+
+        return response
+
+    return wrap
+
+
+@app.get("/", tags=["General"])
+@construct_response
+def _index(request: Request):
+    """Health check."""
+    response = {
+        "message": HTTPStatus.OK.phrase,
+        "status-code": HTTPStatus.OK,
+        "data": {},
+    }
+    return response
