3a0decdd8c3bcebc68ff88eda6e699dab965a3ab
johngiorgi <johnmgiorgi@gmail.com>
2021-02-18 21:27:09 -0500

    :sparkles: Add jump detection endpoint

---
+++ app/schemas.py
@@ -0,0 +1,22 @@
+from typing import List
+
+from pydantic import BaseModel, root_validator
+
+
+class AccelerometerData(BaseModel):
+    x: List[float]
+    y: List[float]
+    z: List[float]
+
+    @root_validator
+    def check_equal_num_timesteps(cls, values):
+        x, y, z = values.get("x"), values.get("y"), values.get("z")
+        len_x, len_y, len_z = len(x), len(y), len(z)
+        if not len(x) == len(y) == len(z):
+            raise ValueError(
+                (
+                    "x, y, and z timesteps must be of equal length."
+                    f" Got {len_x}, {len_y}, and {len_z} respectively."
+                )
+            )
+        return values
