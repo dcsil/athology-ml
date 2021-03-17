55c3e1846bb050c398be0ae9ac79355eccd9a197
johngiorgi <johnmgiorgi@gmail.com>
2021-03-17 10:18:59 -0400

    :recycle: Allow user to specify batch step and timesteps

--- athology_ml/ml/jump_detection/train.py
+++ athology_ml/ml/jump_detection/train.py
@@ -7,8 +7,6 @@ from athology_ml.ml.jump_detection.util import KERAS_TUNER_SEED, print_baselines
 
 app = typer.Typer(callback=set_seeds)
 
-BUFFER_SIZE = 10000
-
 
 @app.command()
 def train(
@@ -18,10 +16,14 @@ def train(
             "A directory that contains three CSVs: train.tsv, valid.tsv and test.tsv that will be"
             " used to train, tune and evaluate the model."
         ),
-    )
+    ),
+    batch_size: int = typer.Option(16, help="Batch size to use during tuning."),
+    num_timesteps: int = typer.Option(128, help="Number of timesteps to include in each example."),
 ) -> None:
     """Train a model to perform jump detection on raw accelerometer data."""
-    train_dataset, valid_dataset, test_dataset = preprocessing.get_datasets(directory)
+    train_dataset, valid_dataset, test_dataset = preprocessing.get_datasets(
+        directory, batch_size, num_timesteps
+    )
     features, labels = preprocessing.get_features_and_labels(train_dataset)
     classifier_bias_init = preprocessing.get_classifier_bias_init(labels)
     normalizer = preprocessing.get_normalizer(features)
@@ -33,32 +35,24 @@ def train(
     # Print baselines to console
     print_baselines(labels)
 
-    # Set the generator up to shuffle data on each epoch.
-    # See https://www.tensorflow.org/api_docs/python/tf/data/Dataset#shuffle
-    train_dataset = train_dataset.shuffle(BUFFER_SIZE, reshuffle_each_iteration=True)
-
     hypermodel = JumpDetector(normalizer=normalizer, classifier_bias_init=classifier_bias_init)
-
-    class_weight_0 = 0.5
-    class_weight_1 = 5
 
     tuner = RandomSearch(
         hypermodel,
-        objective=kerastuner.Objective("val_recall", direction="max"),
-        max_trials=10,
+        objective=kerastuner.Objective("val_precision", direction="max"),
+        max_trials=30,
         seed=KERAS_TUNER_SEED,
         allow_new_entries=True,
         directory="tuning",
-        project_name=f"{class_weight_0}-{class_weight_1}",
+        project_name=f"jump_detection_{batch_size}_{num_timesteps}",
     )
     tuner.search_space_summary()
 
     tuner.search(
         train_dataset,
-        epochs=2,
+        epochs=10,
         # callbacks=callbacks,
         validation_data=valid_dataset,
-        class_weight={0: class_weight_0, 1: class_weight_1},
     )
 
 
