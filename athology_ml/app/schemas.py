84ea6f0ddf5504a5fe660800a5a6d9200179b47f
johngiorgi <johnmgiorgi@gmail.com>
2021-03-21 12:09:04 -0400

    :wrench: Add new schemas for athlete data

--- athology_ml/app/schemas.py
+++ athology_ml/app/schemas.py
@@ -42,3 +42,42 @@ class AccelerometerData(BaseModel):
                 "z": [-0.371, 0.537, 0.738, 0.566, 0.528, 0.531, 0.496, 0.507, 0.542, 0.554, 0.468],
             }
         }
+
+
+class AtheleteJump(BaseModel):
+    load: float
+    height: float
+
+
+class AtheleteSession(BaseModel):
+    date: str
+    timestamp: str
+    jumps: List[AtheleteJump]
+    num_jumps: int
+    max_load: float
+    max_height: float
+
+
+class AtheleteData(BaseModel):
+    _id: str
+    name: str
+    sessions: List[AtheleteSession]
+
+    class Config:
+        # This will be auto-populated as an example in the docs.
+        schema_extra = {
+            "example": {
+                "_id": "28187aa4-89c5-11eb-8dcd-0242ac130003",
+                "name": "Laurie",
+                "sessions": [
+                    {
+                        "date": "30/01/2021",
+                        "timestamp": "1612021861",
+                        "jumps": [{"load": 4.67, "height": 1.23}],
+                        "num_jumps": 1,
+                        "max_load": 4.67,
+                        "max_height": 1.23,
+                    }
+                ],
+            }
+        }
